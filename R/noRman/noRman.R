63d99eff7aee179e0dacd84c1f7208e9d9c95ec8
Excel Que <Excel.Que@gmail.com>
2019-11-13 09:45:03 -0800

    Added offval for small number control

--- R/noRman/noRman.R
+++ R/noRman/noRman.R
@@ -1,4 +1,6 @@
 #One stop shop for normalizing numeric data using logs
+#Note: Yes, this is very old fashioned and is meant to be used
+#when a generalized linear model cannot be fit into your workflow
 
 #Test data
 #library(ALL)
@@ -6,7 +8,7 @@
 #test <- t(exprs(ALL))[,1:100]
 
 
-norMan <- function(x, base = exp(1), offset = FALSE, summary = FALSE){
+norMan <- function(x, base = exp(1), offset = FALSE,offval = 0.000001, summary = FALSE){
   #Check if data is a numeric matrix
   if(!is.matrix(x) | !all(sapply(x, is.numeric))){
     stop("Must submit a numeric matrix")
@@ -15,6 +17,11 @@ norMan <- function(x, base = exp(1), offset = FALSE, summary = FALSE){
   if(any(x <= 0)){
     stop("Matrix contains values zero or less than zero.\n Set offset to TRUE.")
   }
+  #If offset = TRUE, add the minimum + a very small number offval
+  if(offset == TRUE){
+    x <- apply(x,2, function(y) y + abs(min(y))+offval)
+  }
+  
   #Use shapiro test W to determine non-normal columns
   nonNorm <- function(x){
     apply(x, 2, function(x) shapiro.test(x)$statistic < 0.95)
