57329aa14c69b91509b8ec7dd5e4602a6aed0199
Excel Que <34141973+exsquire@users.noreply.github.com>
2020-01-06 15:35:57 -0800

    Added comments and fixed alpha bug

--- R/extilityBelt/enrichRMod.R
+++ R/extilityBelt/enrichRMod.R
@@ -8,21 +8,26 @@ enrichrMod <- function (genes, alpha = 0.05,
   if(quiet == F){
     cat("Uploading data to Enrichr... ")
   }
+  
+  #Call and Response to EnrichR
   if (is.vector(genes)) {
     temp <- POST(url = "http://amp.pharm.mssm.edu/Enrichr/enrich", 
                  body = list(list = paste(genes, collapse = "\n")))
   }
   else {
-    warning("genes must be a vector of gene names or a dataframe with genes and score.")
+    warning("genes must be a vector of gene names.")
   }
   GET(url = "http://amp.pharm.mssm.edu/Enrichr/share")
   if(quiet == F){
     cat("Done.\n")
   }
+  
+  #Load all available databases on enrichR
   dbs <- as.list(databases)
   dfSAF <- options()$stringsAsFactors
   options(stringsAsFactors = FALSE)
   
+  #Pull query results and format
   result <- lapply(dbs, function(x) {
     if(quiet == F){
       cat("  Querying ", x, "... ", sep = "")
@@ -43,22 +48,29 @@ enrichrMod <- function (genes, alpha = 0.05,
   if(quiet == F){
     cat("Parsing results... ")
   }
+  
+  #Name for databases searched
   names(result) <- dbs
   if(quiet == F){
     cat("Done.\n")
   }
   
+  #Cull searches with no results
   nullCull <- function(x, alpha = 0.05){
     #for each slot, check Adjusted.P.Value column
-    #Null if greater than alpha
-    x <- x[x$Adjusted.P.value <= alpha,-c(5,6)]
+    #Null if greater than 0.05
+    if(nrow(x) == 0){
+      x <- NULL
+    }else{
+      x <- x[x$Adjusted.P.value <= alpha,-c(5,6)]
+    }
   }
-  
   res <- lapply(result, nullCull, alpha = alpha)
   if(any(sapply(res,is.null))){
     res <- res[sapply(res,is.null) != TRUE]
   }
     
+  #Cull any empty dataframes after initial filter
   if(sum(sapply(res, nrow)) == 0){
     return(NULL)
   }else{
@@ -70,6 +82,7 @@ enrichrMod <- function (genes, alpha = 0.05,
                                      stringsAsFactors = F),
                           sigRes[[j]])
     }
+    #Output as dataframe
     return(do.call("rbind",outList))
   }
 }
