6f41b74dd3964d77a002053fe24bb7d8a4461450
Excel Que <34141973+exsquire@users.noreply.github.com>
2020-01-07 13:07:12 -0800

    Update bashBoard.sh

--- bash/bashBoard/bashBoard.sh
+++ bash/bashBoard/bashBoard.sh
@@ -1,43 +1,63 @@
 #Build ~/.bashrc custom aliases for cluster use
-#to activate source ~/.bashrc
-
+#TO ACTIVATE MANUALLY  > source ~/.bashrc
+##########BASHBOARD##########
 #Aliases
-#List your current aliases
-alias extility='echo | cat ~/.bashrc | grep -E "alias"'
-
-#Basic commands with safety arguments defaulted
+#Protective functions
 alias rm='rm -i'
 alias cp='cp -i'
 alias mv='mv -i'
 alias chmod='chmod -v'
-
-#ask for currently running jobs, refresh every 10 seconds, ctrl+c to exit
+#Updating job queue
 alias jobs='squeue -u $USER -i10'
+#See cluster traffic
+alias traffic='sinfo --Node --long --sort=PARTITION'
+#cat last file in list
+alias catLast='cat "$(ls -rt | tail -n1)"'
+#Count files in directory
+alias countFiles='ls -1 | wc -l'
+#Check sh symlink
+alias checkShell='file -h /bin/sh'
 
+#FARM-specific
+#Quick R roll ups
+alias ptyR='srun --mem=60000 --time=24:00:00 --partition=high --pty R'
+#Quick Dash roll ups
+alias ptyDash='srun --mem=60000 --time=24:00:00 --partition=high --pty sh'
+
+##CERES-specific
+#Show cluster configuration settings
+alias configs='scontrol show config'
+#Show the account QOS limitations on the cluster
+alias limits='sacctmgr -p list qos $@ format=Name,Priority,GraceTime,GrpTRES,GrpJobs,GrpSubmit,GrpSubmit,MaxTRES,MaxTRESPerUser,MaxJobsPU | column -ts"|"'
+#Show the limits on your own account
+alias mylimits='sacctmgr list associations format=Account,User,Partition,qos'
 #Show specs on last jobs ran - job history usually 24hr but depends on cluster
 alias specs='sacct --format="JobName%30, JobID, Start, Partition, NCPUS, MaxRSS,MaxDiskWrite, Elapsed, STATE" --units=G'
 
-#Shows farm usage across all node
-alias traffic='sinfo --Node --long --sort=PARTITION'
 
-#open the last file in a list, useful for long lists of .out and .err files
-alias catLast='cat "$(ls -rt | tail -n1)"'
+#Functions
+#bashboard - prints everything between BashBoad delimiters
+function bashboard() {
+LOC=$(cat ~/.bashrc | grep -n BASHBOARD | cut -f 1 -d:)
+arr=($LOC)
+FIRST=${arr[0]}
+LAST=${arr[-1]}
+sed -n "${FIRST},${LAST}p" ~/.bashrc
+}
 
-#Count the number of files in current directory
-alias countFiles='ls -1 | wc -l'
+#snoop - pull user data by user name - accepts multiple arguments
+function snoop() { for user in "$@" ; do echo "Snooping on $user" ;  grep "$user" /etc/passwd; printf "\n"; done }
 
-#Show the account QOS limitations on the cluster
-alias limits='sacctmgr -p list qos $@ format=Name,Priority,GraceTime,GrpTRES,GrpJobs,GrpSubmit,GrpSubmit,MaxTRES,MaxTRESPerUser,MaxJobsPU | column -ts"|"'
+#rollUp - user command pseudoterminal request
+function rollUp () {
+printf "Enter Allocation Parameters...\n"
+read -p 'Memory (Mb): ' MEM
+read -p 'Time (hours): ' TIME
+read -p 'Partition: ' PARTITION
+read -p 'Program: ' PROGRAM
+printf "Requesting...\n"
 
-#Show the limits on your own account
-alias mylimits='sacctmgr list associations format=Account,User,Partition,qos'
+srun --mem=$MEM --time="$TIME:00:00" --partition=$PARTITION --pty $PROGRAM
+}
 
-#Show cluster configuration settings
-alias configs='scontrol show config'
-
-#Run interactive R in pseudo terminal on FARM
-alias ptyR='srun --mem=60000 --time=24:00:00 --partition=high --pty R'
-
-#Run interactive bash in pseudo terminal on FARM
-alias ptyBash='srun --mem=60000 --time=24:00:00 --partition=high --pty bash'
-
+##########BASHBOARD###########
